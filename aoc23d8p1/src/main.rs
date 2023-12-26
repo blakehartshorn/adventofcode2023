07c28ecb23ba10943f6a19e200123bf0cf0ce0e8
Blake Hartshorn <contact@blakehartshorn.com>
2023-12-26 12:26:55 -0500

    chore: day 8 part 1

---
+++ aoc23d8p1/src/main.rs
@@ -0,0 +1,92 @@
+use std::fs;
+use std::collections::HashMap;
+
+use regex::Regex;
+
+struct Node {
+    l: String,
+    r: String,
+}
+
+// Totally unnecessary? Makes code later cleaner I think.
+impl Node {
+    fn get(&self, side: char) -> &String {
+        if side == 'L' {
+            &self.l
+        } else {
+            &self.r
+        }
+    }
+}
+
+// For infinitely iterating over the LR string.
+struct Consumer {
+    sides: Vec<char>,
+    i: usize,
+}
+
+impl Consumer {
+    fn new(orly: Vec<char>) -> Consumer {
+        Consumer { 
+            sides: orly,
+            i: 0,
+        }
+    }
+}
+
+impl Iterator for Consumer {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+        if &self.i >= &self.sides.len() {
+            self.i = 0;
+        }
+
+        let side = &self.sides[self.i];
+        self.i += 1;
+
+        Some(*side)
+    }
+}
+
+fn main() {
+
+    let mut network_map: HashMap<String,Node> = HashMap::new();
+    let mut last = String::from("AAA");
+    let mut orly: Vec<char> = Vec::new();
+
+    for (i, line) in fs::read_to_string("input.txt")
+            .unwrap().lines().enumerate() {
+
+        if i == 0 {
+            orly = line.chars().collect();
+            continue;
+        }
+
+        if !line.contains("=") {
+            continue;
+        }
+
+        let re = Regex::new(r"([A-Z])\w+").unwrap();
+        let directions: Vec<&str> = re.find_iter(line)
+            .map(|m| m.as_str()).collect();
+
+        network_map.insert(directions[0].to_string(), Node { 
+            l: directions[1].to_string(),
+            r: directions[2].to_string(),
+        });
+    }
+
+    let mut steps: i32 = 0;
+    let mut orlynow = Consumer::new(orly);
+    loop {
+        steps += 1;
+        let next_side = orlynow.next().unwrap();
+        last = network_map[&last].get(next_side).clone();
+        if last == "ZZZ".to_string() {
+            println!("{}", steps);
+            break;
+        }
+    }
+}
